17d5f8d9f1ff32ed1480f88ca72bd4111a2f3c9b
amadeep <aman.2.deep.ext@nokia.com>
2025-07-19 23:30:45 +0530

    adding calc

---
+++ calc.py
@@ -0,0 +1,13 @@
+# calc.py
+
+# prompt the user (string prompt), then convert the returned text to int
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+# compute and print the sum
+sum = a + b
+print("Sum is:", sum)
+
+sub = a - b
+print("Sub:",sub)
+
